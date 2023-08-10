3afeb1e819933b9234d844d94b33a9682b2d71ee
Simphiwe06 <simphiwembuli06@gmail.com>
2023-08-10 02:15:38 +0200

    fixed a few error to achieve  the intended outcome

--- scripts.js
+++ scripts.js
@@ -1,33 +1,29 @@
 const FREE_WARNING = 'Free shipping only applies to single customer orders';
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
 
-// Initial variable declarations
-let location = 'RSA';
+let loc = 'RSA'; // Renamed the variable to 'loc'
 let currency = 'R';
 let shipping = null;
 let customers = 1;
 
-// Function to calculate shipping cost based on location
 function calculateShipping(location) {
-  if (location === 'RSA') {
+  if (loc === 'RSA') { // Changed 'location' to 'loc'
     shipping = 400;
-  } else if (location === 'NAM') {
+  } else if (loc === 'NAM') { // Changed 'location' to 'loc'
     shipping = 600;
   } else {
     shipping = 800;
   }
 }
 
-// Function to check if free shipping applies
 function checkFreeShipping(totalCost, location, customers) {
-  if (totalCost >= 1000 && (location === 'RSA' || location === 'NAM') && customers === 1) {
+  if (totalCost >= 1000 && (loc === 'RSA' || loc === 'NAM') && customers === 1) { // Changed 'location' to 'loc'
     shipping = 0;
   } else if (shipping === 0 && customers !== 1) {
     console.log(FREE_WARNING);
   }
 }
 
-// Function to calculate the total cost
 function calculateTotalCost(shoes, toys, shirts, batteries, pens) {
   return shoes + toys + shirts + batteries + pens;
 }
@@ -35,23 +31,18 @@ function calculateTotalCost(shoes, toys, shirts, batteries, pens) {
 // Main code block
 const shoes = 300 * 1;
 const toys = 100 * 5;
-const shirts = 150 * 0; // 'NONE_SELECTED' replaced with 0
+const shirts = 150 * 0;
 const batteries = 35 * 2;
-const pens = 5 * 0; // 'NONE_SELECTED' replaced with 0
+const pens = 5 * 0;
 
-// Calculate the total cost
 const totalCost = calculateTotalCost(shoes, toys, shirts, batteries, pens);
 
-// Check if location is North Korea
-if (location === 'NK') {
+if (loc === 'NK') { // Changed 'location' to 'loc'
   console.log(BANNED_WARNING);
 } else {
-  // Calculate the shipping cost based on location
-  calculateShipping(location);
+  calculateShipping(loc); // Changed 'location' to 'loc'
 
-  // Check if free shipping applies
-  checkFreeShipping(totalCost, location, customers);
+  checkFreeShipping(totalCost, loc, customers); // Changed 'location' to 'loc'
 
-  // Log the final price and currency to the console
   console.log('Price:', currency, totalCost + shipping);
 }
